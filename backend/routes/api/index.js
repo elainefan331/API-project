5bcf75f4ed5df9eab92279316fccfd267659c528
elaine <elainefan331@gmail.com>
2024-01-25 19:37:02 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,44 +1,21 @@
 const router = require('express').Router();
-
-// D4 phase3: test restoreUser middleware
-// GET /api/restore-user
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
+
 router.use(restoreUser);
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-// D3 test route
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-// });
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
-// D4 phase3: test setTokenCookie middleware
-// GET /api/set-token-cookie
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
 
-// D4 phase3: test requireAuth middleware
-// GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+
+
 
 module.exports = router;
